28ea6ec86f6d849c9981ee7ec22c25b6a8cd78f8
nekoshiro046 <nekoshiro046@gmail.com>
2020-04-24 02:55:06 +0900

    logo animation updata

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(function() {
 	$('html,body').animate({ scrollTop: 0 }, '1');
 	$("nav").hide().fadeIn(5000);
+	new Vivus('canvas', {start: 'autostart', type: 'delayed', duration: 150}); 
+	// new Vivus('canvas', {start: 'autostart', type: 'scenario-sync', duration: 25});
 });
+
 var navBtnActive = 0;//0:閉じ 1:開く
 $(document).on('click','.menuIcon', function() {
 	if(navBtnActive == 0){
@@ -34,6 +37,13 @@ $('.menu').on('click',function(){
 	if(navBtnActive == 1){
 		$('nav').removeClass('open');
 		$('nav').css('position','absolute');
+		$('.menuIcon').css('color','#000');
+		$('.menu__button-bottom').css('fill','#000');
+		$('.menu__button-top').css('fill','#000');
+		$('.menu__button-bottom').css('animation-name','closeAnimeBottom');
+		$('.menu__button-bottom').css('animation-duration','0.5s');
+		$('.menu__button-top').css('animation-name','closeAnimeTop');
+		$('.menu__button-top').css('animation-duration','0.5s');
 		$('.blackGround').css('display','none');
 		navBtnActive = 0;
 	}
@@ -45,11 +55,14 @@ $(window).scroll(function(){
 	if(scroll > windowHeight*3/4){
 		if(navBtnActive == 0 && !changed){
 			$("#top").html('<h1 class="menu_Logo"><a class = "menuIcon"><div class="menu__button"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-4 -3.762 29.76 43.882" xml:space="preserve"><g><polygon class="menu__button-bottom" points="0,7.4 0,22.2 14.7,22.2"></polygon></g><g><polygon class="menu__button-top" points="22,14.8 22,0 7.3,0"></polygon></g></svg></div>menu</a></h1>');
-			$("#top").hide().fadeIn(500);
+			$("#top").hide().fadeIn(1000);
 			changed = true;
 		}
 	} else {
-		$("#top").html('<h1 class="menu_Logo">卓展<br>2020</h1>');
-		changed = false;
+		if(navBtnActive == 0 && changed){
+			$("#top").html('<h1 class="menu_Logo">卓展<br>2020</h1>');
+			$("#top").hide().fadeIn(1000);
+			changed = false;
+		}
 	}
 })
