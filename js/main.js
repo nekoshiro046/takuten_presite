424423ee80abba477f86d596163ca67bdb623e55
nekoshiro046 <nekoshiro046@gmail.com>
2020-04-26 04:43:29 +0900

    responsib updata 4 pc

--- js/main.js
+++ js/main.js
@@ -8,8 +8,12 @@ $(function() {
     },1000);
 	// new Vivus('canvas', {start: 'autostart', type: 'scenario-sync', duration: 25});
 });
-function empty(){
-
+var windowWidth = jQuery(window).width();
+var menuIconVisible;
+if(windowWidth > 800){
+	menuIconVisible = false;
+}else{
+	menuIconVisible = true;
 }
 var navBtnActive = 0;//0:閉じ 1:開く
 $(document).on('click','.menuIcon', function() {
@@ -56,19 +60,21 @@ $('.menu').on('click',function(){
 });
 var changed = false;
 $(window).scroll(function(){
-	var scroll = $(document).scrollTop();
-	var windowHeight = jQuery(window).height();
-	if(scroll > windowHeight*3/4){
-		if(navBtnActive == 0 && !changed){
-			$("#top").html('<h1 class="menu_Logo"><a class = "menuIcon"><div class="menu__button"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-4 -3.762 29.76 43.882" xml:space="preserve"><g><polygon class="menu__button-bottom" points="0,7.4 0,22.2 14.7,22.2"></polygon></g><g><polygon class="menu__button-top" points="22,14.8 22,0 7.3,0"></polygon></g></svg></div>menu</a></h1>');
-			$("#top").hide().fadeIn(1000);
-			changed = true;
-		}
-	} else {
-		if(navBtnActive == 0 && changed){
-			$("#top").html('<h1 class="menu_Logo">卓展<br>2020</h1>');
-			$("#top").hide().fadeIn(1000);
-			changed = false;
+	if(menuIconVisible){
+		var scroll = $(document).scrollTop();
+		var windowHeight = jQuery(window).height();
+		if(scroll > windowHeight*3/4){
+			if(navBtnActive == 0 && !changed){
+				$("#top").html('<h1 class="menu_Logo"><a class = "menuIcon"><div class="menu__button"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-4 -3.762 29.76 43.882" xml:space="preserve"><g><polygon class="menu__button-bottom" points="0,7.4 0,22.2 14.7,22.2"></polygon></g><g><polygon class="menu__button-top" points="22,14.8 22,0 7.3,0"></polygon></g></svg></div>menu</a></h1>');
+				$("#top").hide().fadeIn(1000);
+				changed = true;
+			}
+		} else {
+			if(navBtnActive == 0 && changed){
+				$("#top").html('<h1 class="menu_Logo">卓展<br>2020</h1>');
+				$("#top").hide().fadeIn(1000);
+				changed = false;
+			}
 		}
 	}
 })
